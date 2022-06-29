4e147750aad00623cb83de6baac46824250b550c
sunyoung <cabal79@naver.com>
2022-06-29 13:37:49 +0900

    modify App.js state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React,{Component} from 'react';
 import TOC from './components/TOC'
@@ -7,11 +7,22 @@ import Content from './components/Content'
 
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      Subject:{title : 'WEB', sub : 'World Wid Web'},
+      contents:[
+        {id:1,title:'HTML', desc:"HTML is for imformation"},
+        {id:2,title:'CSS', desc:"CSS is for designn"},
+        {id:3,title:'JavaScript', desc:"JavaScript is for interactive"}
+      ]
+    }
+  }
   render(){
   return (
     <div>
-      <Subject title="WEB" sub="world wide web"></Subject> {/* <Subject />  */}
-      <TOC></TOC>
+      <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject> {/* <Subject />  */}
+      <TOC data={this.state.contents}></TOC>
       <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
     </div>
   );
