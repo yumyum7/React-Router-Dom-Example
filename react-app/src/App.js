823439b2255070fc59d093d852362aafaeec159e
sunyoung <cabal79@naver.com>
2022-06-29 10:38:15 +0900

    create react management

---
+++ react-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{Component} from 'react';
+import TOC from './components/TOC'
+import Subject from './components/Subject'
+import Content from './components/Content'
+
+
+class App extends Component{
+  render(){
+  return (
+    <div>
+      <Subject title="WEB" sub="world wide web"></Subject> {/* <Subject />  */}
+      <TOC></TOC>
+      <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+    </div>
+  );
+ }
+}
+
+export default App;
