d61f91f902257803a7a4eeb38b393a0e04dce70a
sunyoung <cabal79@naver.com>
2022-06-30 17:41:49 +0900

    create update.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,96 +1,138 @@
 
 import './App.css';
-import React,{Component} from 'react';
+import React,{ Component } from 'react';
 import TOC from './components/TOC'
-import Subject from './components/Subject'
-import ReadContent from './components/ReadContent'
-import Control from './components/Control'
-import CreateContent from './components/CreatContent';
+import Subject from './components/Subject';
+import ReadContent from './components/ReadContent';
+import Control from './components/Control';
+import CreateContent from './components/CreateContent';
 import UpdateContent from './components/UpdateContent';
-
 
 class App extends Component{
   constructor(props){
     super(props);
     this.max_content_id = 3;
     this.state = {
-      mode : 'read', 
+      mode:'read',
       selected_content_id:2,
-      Subject : {title : 'WEB', sub : 'World Wid Web'},
-      welcome : {title:'Welcome', desc:'Hello, React!!'},
+      subject:{title:'WEB',sub:'World Wid Web'},
+      welcome:{title:'Welcome',desc:'Hello, React!!'},
       contents:[
-        {id:1,title:'HTML', desc:"HTML is for imformation"},
-        {id:2,title:'CSS', desc:"CSS is for designn"},
-        {id:3,title:'JavaScript', desc:"JavaScript is for interactive"}
+        {id:1,title:'HTML',desc:'HTML is for imformation'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
       ]
     }
   }
-  render(){
-    console.log('App render');
+
+  //getReadContent start
+
+  getReadContent(){
+    let i = 0;
+      while(i < this.state.contents.length) {
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          return data;
+        }
+        i = i + 1;
+      }
+  }
+
+  //getReadContent end
+
+  //getContent start
+  
+  getContent(){
     let _title, _desc = null, _article;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     }else if(this.state.mode === 'read'){
-      let i = 0;
-      while(i<this.state.contents.length){
-        let data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+      let _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>;
     }else if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title,_desc){
-          this.max_content_id = this.max_content_id + 1;
-       //   this.state.contents.push(
-       //     {id:this.max_content_id,title:_title,desc:_desc}
-       //   );  push는 원본, concat은 복사본
+        this.max_content_id = this.max_content_id + 1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id,title:_title,desc:_desc}
+        // );
 
-          let _contents = this.state.contents.concat(
-             {id:this.max_content_id,title:_title,desc:_desc}
-           );
+        //let _contents = this.state.contents.concat(
+        //     {id:this.max_content_id,title:_title,desc:_desc}
+       //  );
 
-          this.setState({
-            //contents: this.state.contents
-            contents : _contents
-          })
+       let _contents = Array.from(this.state.contents);
+       _contents.push({id:this.max_content_id,title:_title,desc:_desc});
+
+        this.setState({
+          //contents : this.state.contents
+          mode:'read',
+          contents : _contents,
+          selected_content_id:this.max_content_id
+        })
       }.bind(this)}></CreateContent>
+    }else if(this.state.mode === 'update'){
+      let _content = this.getReadContent();
+      _article = <UpdateContent 
+                      data = {_content} 
+                      onSubmit={function(_id,_title,_desc){
+                         let _contents = Array.from(this.state.contents); 
+                         let i =0;
+                         while(i < _contents.length){
+                          if(_contents[i].id === _id){
+                            _contents[i]={id:_id, title:_title, desc:_desc};
+                            break;
+                          }
+                          i=i+1;
+                         }
+                         this.setState({
+                          contents:_contents,
+                          mode:'read'
+                         })
+                      }.bind(this)}>
+
+                  </UpdateContent>
     }
-  return (
-    <div>
-      <Subject 
-        title={this.state.Subject.title} 
-        sub={this.state.Subject.sub}
-        onChangePage={function(){
-          this.setState({mode:'welcome'})
-        }.bind(this)}
-      >
-      </Subject> 
-      <TOC data={this.state.contents}
-          onChangePage={function(id){
-          this.setState(
-            {
-              mode:'read',
-              selected_content_id : Number(id)
-            }
-            );
+
+    return _article;
+  }
+
+  //getContent end
+
+  render(){
+    console.log('App render');
+
+    return (
+      <div>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub} 
+          onChangePage={function(){
+            this.setState({mode:'welcome'}) ;
           }.bind(this)}
         >
-      </TOC>
-      <Control onChangeMode={function(_mode){
-        this.setState({
-          mode:_mode
-        });
-      }.bind(this)}></Control>
-      {_article}
-    </div>
-  );
- }
+        </Subject>
+        <TOC data={this.state.contents}
+          onChangePage={function(id){
+            this.setState(
+              {
+                mode:'read',
+                selected_content_id:Number(id)
+              }
+            ) ;
+          }.bind(this)}
+        >
+        </TOC>
+        <Control onChangeMode={function(_mode){
+            this.setState({
+              mode:_mode
+            });
+          }.bind(this)}></Control>
+        {this.getContent()}
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-app/src/components/UpdateContent.js
+++ react-app/src/components/UpdateContent.js
@@ -1,10 +1,22 @@
-import React,{Component} from 'react';
+import React,{ Component } from 'react';
 
+class UpdateContent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            id:this.props.data.id,
+            title : this.props.data.title,
+            desc:this.props.data.desc
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
 
-class UpdateContent extends Component{
-    render(){
+    inputFormHandler(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
+    render() {
       console.log('UpdateContent render');
-      return(
+      return (
         <article>
             <h2>Update</h2>
             <form 
@@ -12,17 +24,29 @@ class UpdateContent extends Component{
                 method="post" 
                 onSubmit={function(e){
                     e.preventDefault();
-                    this.props.onSubmit(e.target.title.value, e.target.desc.value);
+                    this.props.onSubmit(this.state.id,this.state.title,this.state.desc);
                 }.bind(this)}
-            > 
-                <p><input type="text" name="title" placeholder="title"></input></p>
-                <p><textarea name="desc" placeholder="description"></textarea></p>
-                <p><input type="submit"></input></p>
+            >
+                <input type="hidden" name="id" value={this.state.id}></input>
+                <p><input 
+                    type="text" 
+                    name="title" 
+                    placeholder="title"
+                    value={this.state.title}
+                    onChange={this.inputFormHandler}
+                ></input>
+                </p>
+                <p><textarea 
+                    name='desc' 
+                    placeholder='description' 
+                    value={this.state.desc}
+                    onChange={this.inputFormHandler}
+                ></textarea></p>
+                <p><input type='submit'></input></p>
             </form>
         </article>
       );
     }
   }
-
 
   export default UpdateContent;
